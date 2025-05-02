708f7e4089374b20695a9ada768ef64e5dc50d37
Florencia Scolari <florscolari0@gmail.com>
2025-05-02 20:33:35 +0800

    added validation for q1 and calculation has been made in second activity

--- app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/MainActivity.kt
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/MainActivity.kt
@@ -1,20 +1,30 @@
 package au.edu.kbs.mobiledevelopment.tech4300a2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnMainToSecond = findViewById<Button>(R.id.btn_goto_second_activity)
+        val q1Answer = findViewById<EditText>(R.id.q1UserValue)
 
         btnMainToSecond.setOnClickListener{
             val mainToSecond = Intent(this, SecondActivity::class.java)
+            // Taking values from user input elements
+            val q1AnswerValue = q1Answer.text.toString()
+
+            // Time to send data
+            mainToSecond.putExtra("KEY_Q1_ANSWER", q1AnswerValue )
+
             startActivity(mainToSecond)
         }
 
--- app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/SecondActivity.kt
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/SecondActivity.kt
@@ -3,6 +3,7 @@ package au.edu.kbs.mobiledevelopment.tech4300a2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
@@ -16,5 +17,36 @@ class SecondActivity : AppCompatActivity() {
             val secondToMain = Intent(this, MainActivity::class.java)
             startActivity(secondToMain)
         }
+
+        // Total Score, Marks & Wrong answers
+        var totalScore = 0
+        val marks : Int
+        val wrongAnswers : Int
+
+
+        // Retrieving data from Main Activity
+        val i = intent
+        val mainQ1Answer = i.getStringExtra("KEY_Q1_ANSWER")?.toInt()
+
+        // Functions to check data & evaluate results
+        val result: String
+        val resultCorrect = 4
+       if (mainQ1Answer == resultCorrect) {
+           result = "Correct"
+           totalScore += 1
+        } else
+           result = "Incorrect"
+            totalScore -= 1
+
+
+
+
+
+        // Assigning data to UI elements to be displayed on Main Activity
+        val textResult = findViewById<TextView>(R.id.result)
+        textResult.text = result
+
+        // todo: add an element to display totalScore
+
     }
 }
