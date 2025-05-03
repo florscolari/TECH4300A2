4b77b0a971cd26f74f14080017ce931df055cee3
Florencia Scolari <florscolari0@gmail.com>
2025-05-03 14:32:21 +0800

    added Question and TrueOfFalseQType classes

--- app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/MainActivity.kt
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/MainActivity.kt
@@ -1,11 +1,12 @@
 package au.edu.kbs.mobiledevelopment.tech4300a2
 
 
+import Question
+import TrueOfFalseQType
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +14,14 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
+// Setting questions by using Question class
+val question1 = Question(1, "Is this operation correct?", "True")
+val question2 = TrueOfFalseQType(1, "Is this operation correct?", "True", "6 - 1 = 8", "Choose between True or False")
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/Question.kt
@@ -0,0 +1,48 @@
+
+class Question (
+    private val number: Int,
+    private val label: String,
+    private val answer: String,
+) {
+    open fun getNumber(): Int {
+        return number
+    }
+
+    open fun getLabel(): String {
+        return label
+    }
+
+    open fun getAnswer(): String {
+        return answer
+    }
+}
+
+class TrueOfFalseQType(
+    private val number: Int,
+    private val label: String,
+    private val answer: String,
+    private val operationText : String,
+    //private val option : Array,
+    private val feedback : String,
+    )
+ {
+    fun getTrueOption () : String {
+        val option : String = "True"
+        return option
+    }
+
+     fun getFalseOption () : String {
+         val option : String = "False"
+         return option
+     }
+
+
+}
+
+
+fun main() {
+    val question1 = TrueOfFalseQType(1, "Is this operation correct?", "True", "5 + 4 = 9", "feedback")
+    val q1Answer = question1.getTrueOption()
+    println(q1Answer)
+
+}
