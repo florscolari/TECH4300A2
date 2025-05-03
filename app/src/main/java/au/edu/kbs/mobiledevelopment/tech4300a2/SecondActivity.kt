15efe4d325e9e981b3df57151c10ac559ae0802e
Florencia Scolari <florscolari0@gmail.com>
2025-05-03 10:54:17 +0800

    added total score, marks and wrong answers counters in 2nd activity

--- app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/SecondActivity.kt
+++ app/src/main/java/au/edu/kbs/mobiledevelopment/tech4300a2/SecondActivity.kt
@@ -11,33 +11,41 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+
+        // Grabbing UI elements from Second Activity
         val btnSecondToMain = findViewById<Button>(R.id.btn_goto_main_activity)
+        val score = findViewById<TextView>(R.id.totalScore)
+        val marksContent = findViewById<TextView>(R.id.marks)
+        val wrongAnswersContent = findViewById<TextView>(R.id.wrongAnswers)
 
         btnSecondToMain.setOnClickListener{
             val secondToMain = Intent(this, MainActivity::class.java)
             startActivity(secondToMain)
         }
 
+
         // Total Score, Marks & Wrong answers
         var totalScore = 0
-        val marks : Int
-        val wrongAnswers : Int
+        var marks = 0
+        var wrongAnswers = 0
 
 
         // Retrieving data from Main Activity
         val i = intent
         val mainQ1Answer = i.getStringExtra("KEY_Q1_ANSWER")?.toInt()
 
-        // Functions to check data & evaluate results
+        // Q1: Check data & evaluate results
         val result: String
         val resultCorrect = 4
-       if (mainQ1Answer == resultCorrect) {
+        if (mainQ1Answer == resultCorrect) {
            result = "Correct"
-           totalScore += 1
-        } else
-           result = "Incorrect"
-            totalScore -= 1
-
+           totalScore ++
+           marks ++
+        } else {
+            result = "Incorrect"
+            totalScore = 0
+            wrongAnswers += 1
+        }
 
 
 
@@ -45,8 +53,12 @@ class SecondActivity : AppCompatActivity() {
         // Assigning data to UI elements to be displayed on Main Activity
         val textResult = findViewById<TextView>(R.id.result)
         textResult.text = result
+        score.text = totalScore.toString()
+        marksContent.text = marks.toString()
+        wrongAnswersContent.text = wrongAnswers.toString()
 
-        // todo: add an element to display totalScore
+
+
 
     }
 }
